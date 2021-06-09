bd6ffe4921f6e802846c62bb366bcb529d747afa
Михаил Сычев <misha@MacBook-Pro-Mihail.local>
2021-06-10 02:20:02 +0300

    Внес правку

--- project/src/components/places-list/places-list.jsx
+++ project/src/components/places-list/places-list.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 function PlacesList({places}) {
   return (
     <div className="cities__places-list places__list tabs__content">
-      {places.map((name, i) => <PlaceCard key={name} />)}
+      {places.map((place) => <PlaceCard key={place} />)}
     </div>
   );
 }
